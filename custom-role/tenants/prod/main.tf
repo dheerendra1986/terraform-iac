1da805d44f411c2f722f5f7315df237228fe37e9
Dheerendra Kumar <dheerendra.kumar@ingka.com>
2022-07-24 10:43:39 +0530

    Running Terraform Create using pipeline

--- custom-role/tenants/prod/main.tf
+++ custom-role/tenants/prod/main.tf
@@ -10,26 +10,19 @@ module "custom-role-project" {
 
   target_level         = "project"
   target_id            = var.project_id
-  role_id              = "iamrolefordeleter"
+  role_id              = "iamroleforproddelete"
   base_roles           = ["roles/iam.serviceAccountAdmin"]
   permissions          = ["iam.roles.list", "iam.roles.create", "iam.roles.delete"]
   excluded_permissions = ["iam.serviceAccounts.setIamPolicy", "resourcemanager.projects.get", "resourcemanager.projects.list"]
   description          = "This is a project level custom role."
-  members              = ["serviceAccount:custom-role-account-01@${var.project_id}.iam.gserviceaccount.com", "serviceAccount:custom-role-account-02@${var.project_id}.iam.gserviceaccount.com"]
+  members              = ["serviceAccount:${var.service_account_name}@${var.project_id}.iam.gserviceaccount.com"]
 }
 
 /******************************************
   Create service accounts to use as members
  *****************************************/
 resource "google_service_account" "custom_role_account_prod" {
-  account_id = "custom-role-account-prod"
+  account_id = var.service_account_name
   project    = var.project_id
 }
-resource "google_service_account" "custom_role_account_01" {
-  account_id = "custom-role-account-01"
-  project    = var.project_id
-}
-resource "google_service_account" "custom_role_account_02" {
-  account_id = "custom-role-account-02"
-  project    = var.project_id
-}
+
