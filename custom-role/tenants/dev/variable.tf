9c669f80c517659be58641aa7730f6258cbbd8cc
Dheerendra Kumar <dheerendra.kumar@ingka.com>
2022-07-24 09:48:22 +0530

    Running Terraform Create using pipeline

--- custom-role/tenants/dev/variable.tf
+++ custom-role/tenants/dev/variable.tf
@@ -4,15 +4,15 @@ variable "project_id" {
   default     = "ingka-icf-extcustdata-test"
 }
 
-variable "service_account_name_01"{
-  type = string
+variable "service_account_name_01" {
+  type        = string
   description = "Name of the service account"
-  default = "custom_role_account_dev_test01"
+  default     = "custom-role-account-dev-test01"
 
 }
 
-variable "service_account_name_02"{
-  type = string
+variable "service_account_name_02" {
+  type        = string
   description = "Name of the service account"
-  default = "custom_role_account_dev_test02"
+  default     = "custom-role-account-dev-test02"
 }
