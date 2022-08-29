0de7e392713dd984c002508d153105700fad9de8
Dheerendra Kumar <dheerendra.kumar@ingka.com>
2022-08-29 16:23:50 +0530

    Comment added for bq table access

---
+++ tenants/bq_iam_permissions/main.tf
@@ -0,0 +1,9 @@
+module "bq_table_iam_member_viewer" {
+  source             = "../../modules/bigquery-table-iam-access"
+  project_id         = "ingka-icf-extcustdata-test"
+  dataset_id         = "event_logs"
+  bigquery_table_id  = ["invoice", "invoices"]
+  account_type       = "user" # account-type - user,serviceAccount,group,domain
+  user_email_address = "amol.sudam.rathod1@ingka.com"
+  user_role          = "roles/bigquery.dataEditor"
+}
