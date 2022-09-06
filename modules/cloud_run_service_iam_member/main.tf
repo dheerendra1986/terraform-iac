cb7dfad265714ea5916b307ae7caf707c0212a8b
Dheerendra Kumar <dheerendra.kumar@ingka.com>
2022-09-06 16:58:15 +0530

    cloud run service iam member change

--- modules/cloud_run_service_iam_member/main.tf
+++ modules/cloud_run_service_iam_member/main.tf
@@ -1,1 +1,8 @@
-#WIP
+resource "google_cloud_run_service_iam_member" "member_cloud_run" {
+  count    = length(var.user_role)
+  location = var.location
+  project  = var.project_name
+  service  = var.cloud_run_name
+  role     = var.user_role[count.index]
+  member   = "${var.account_type}:${var.user_email_address}"
+}
---
+++ modules/cloud_run_service_iam_member/vars.tf
@@ -0,0 +1,18 @@
+variable "project_name" {
+
+}
+variable "user_role" {
+  type = list(any)
+}
+variable "account_type" {
+}
+
+variable "user_email_address" {
+
+}
+variable "location" {
+
+}
+variable "cloud_run_name" {
+
+}
